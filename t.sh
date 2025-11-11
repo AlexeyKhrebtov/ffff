169badc30179be18f29270557da44aad3af5bdb7
Alexey Khrebtov <alexeymegaex@gmail.com>
2025-11-11 13:45:40 +0300

    merged github,gitlab and local changes

--- t.sh
+++ t.sh
@@ -2,12 +2,16 @@
 set -euo pipefail
 echo "syncing.."
 
+git add -u .
+
 if git diff --cached --quiet; then
     echo 'nothing'
 else
     echo "NEED commit !!!"
+    git commit -m 'merged github,gitlab and local changes'
 fi
 
 if [ "$(git rev-list origin/master..HEAD)" ]; then
 	echo "PUSH"
+	git push
 fi
