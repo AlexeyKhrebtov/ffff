ad649df53ceeaa0de896ac87dc4b20beb6ad3b13
Alexey <alexeymegaex@gmail.com>
2025-11-12 17:03:34 +0300

    Update response message in hello route

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,5 +1,5 @@
 routerAdd("GET", "/hello/{name}", (e) => {
     let name = e.request.pathValue("name")
 
-    return e.json(200, { "message": "Hello 1 " + name })
+    return e.json(200, { "message": "Hello 2 " + name })
 })
