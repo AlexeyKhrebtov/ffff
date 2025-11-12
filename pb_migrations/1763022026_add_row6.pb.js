109036fa382c2b4bd264a15465236a9d0e6b2aa1
Alexey <alexeymegaex@gmail.com>
2025-11-12 17:04:40 +0300

    Create 1763022026_add_row6.pb.js

---
+++ pb_migrations/1763022026_add_row6.pb.js
@@ -0,0 +1,25 @@
+/// <reference path="../pb_types.d.ts" />
+
+migrate(
+  (app) => {
+    console.log("[MIGRATION] Start adding department row");
+
+    // Найдём коллекцию departments
+    departments = app.findCollectionByNameOrId("departments");
+    
+    if (!departments) {
+      console.log("⚠️ Collection 'departments' not found, skipping");
+      return;
+    }
+
+    let record = new Record(departments)
+    
+    record.set("name", "6")
+    
+    console.log("✅ Added row");
+    app.save(record);
+  },
+  (app) => {
+    
+  }
+);
